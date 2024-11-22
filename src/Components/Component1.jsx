a5492a5d277a33cb6b73a171fb73298017e83034
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-22 12:39:21 -0500

    Fragments in React

---
+++ src/Components/Component1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Component2 from './Component2'
+
+export default function Component1() {
+    return (
+        <>
+            <h2>This is Component 1</h2>
+            <Component2 />
+        </>
+    )
+}
---
+++ src/Components/Component2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Component2() {
+    return (
+        <>
+            <h3>This is Component 2</h3>
+            <p>This is a paragraph</p>
+        </>
+    )
+}
